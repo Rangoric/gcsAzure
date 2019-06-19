4f83d4df82e36445069e0876bfa0139de92893f4
Wilson Mead III <kem@rangoric.com>
2019-06-19 07:52:32 -0400

    Requirements added to cards.

--- data/Card.cs
+++ data/Card.cs
@@ -1,7 +1,13 @@
 namespace gcsShared.Data
 {
+  public class CardRequirement
+  {
+    public string tag { get; set; }
+    public long value { get; set; }
+  }
   public class Card : BasicItem
   {
+    public CardRequirement[] requirements { get; set; }
     public long value { get; set; }
 
     public string[] tags { get; set; }
